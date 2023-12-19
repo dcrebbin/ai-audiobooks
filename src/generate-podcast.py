a076ccc347b67f012c546e4a2296bb990d19ae74
Devon Crebbin <devon@artvuu.group>
2023-12-19 21:15:02 +1100

    refactored project use different ai providers

--- src/generate-podcast.py
+++ src/generate-podcast.py
@@ -1,74 +1,54 @@
-import os
 import requests
 import re
-from dotenv import load_dotenv
+import sys
+import ai_provider_settings
 
 # Load environment variables from .env file
-load_dotenv()
-
-# API and Audio Settings
-# Model ID for Elevenlabs text-to-speech API
-MODEL_ID = "eleven_multilingual_v2"
 
 # File extension for generated audio
 EXTENSION = ".mp3"
 
-# Voice ID for the "ASMR" voice
-VOICE_ID = "piTKgcLEGmPE4e6mEKli"
+try:
+    # Get AI argument from command line
+    # Specify which AI to use to generate the audio: elevenlabs/openai/unrealspeech
+    SELECTED_AI_ARGUMENT = sys.argv[1]
+except IndexError:
+    print("Please provide an AI argument")
+    print(
+        "Usage: python3 src/determine-cost.py [elevenlabs/openai/unrealspeech]")
+    sys.exit()
 
-# API endpoint with voice ID
-API_URL = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
-
-# Request Headers
-# Includes API key from environment variables
-# And sets content type to JSON
+# Generic request headers
 headers = {
     "Content-Type": "application/json",
-    "xi-api-key": os.getenv("ELEVEN_API_KEY")
+    "Accept": "audio/mp3",
 }
 
-# Function to generate audio from text
-
+# Function for generating audio from text
 async def generate_voice(text, index):
-    """
-    Uses the Elevenlabs API to generate audio file from input text
-
-    Parameters:
-        text (str): The text to convert to speech
-        index (int): The index value for naming output file
-
-    Returns:
-        None 
-    """
-
     print(f"Generating voice for: {index}")
 
-    # API payload with text, voice model and settings
-    payload = {
-        "model_id": MODEL_ID,
-        "text": text,
-        "voice_settings": {
-            "similarity_boost": 0.95,
-            "stability": 0.85
-        }
-    }
+    # Get the AI provider specific generation endpoint and payload
+    API_URL = ai_provider_settings.get_url(SELECTED_AI_ARGUMENT)
+    PAYLOAD = ai_provider_settings.get_payload(SELECTED_AI_ARGUMENT, text)
 
-    # Make API request
-    response = requests.post(API_URL, json=payload, headers=headers)
+    # Update header with AI provider specific authentication
+    auth_header = ai_provider_settings.get_auth(SELECTED_AI_ARGUMENT)
+    headers[auth_header["type"]] = auth_header["value"]
+
+    print(f"Sending request to: {API_URL}")
+    response = requests.post(API_URL, json=PAYLOAD, headers=headers)
 
     # Check for errors
     if response.status_code != 200:
         print(f"Error: {response.text}")
-
     else:
         print(f"Writing audio file for index: {index}")
-
         # Write audio content to file
         with open(f'./output/chunks/test_data{index}{EXTENSION}', 'wb') as file:
             file.write(response.content)
 
 # Main function
-
 async def main():
     # Open text file with UTF-8 encoding
     with open('./data/test_data.txt', 'r', encoding='utf-8') as file:
@@ -89,7 +69,6 @@ async def main():
             await generate_voice(combined_sentence, index)
             print(combined_sentence)
     print("Finished generation! Don't forget to run python ./src/merge-podcast.py to combine all audio files 🤠")
-
 
 if __name__ == "__main__":
     import asyncio
