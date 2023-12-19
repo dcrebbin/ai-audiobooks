4fde63c8291bdc45ee28856c70a52bf288c2d327
Devon Crebbin <devon@artvuu.group>
2023-12-19 21:14:30 +1100

    updated generation estimation time

--- src/determine-generation-time.py
+++ src/determine-generation-time.py
@@ -1,8 +1,32 @@
+import sys
 # Estimate the time it will take to generate a given text file
-# I've done very limited testing but have estimated the ElevenLabs generation speed
-# To be around 16.5 characters per second
-# This is heavily dependent on the length of the text, the voice model used and 11labs current server load
-characters_per_second = 16.5
+# This is heavily dependent on the length of the text, the voice model used, other specific settings and AI provider's current server load
+# These values are based on the average time it took to generate the test_data.txt file using each AI provider (with minimal testing)
+# These values are not guaranteed to be accurate, but are a good guideline for estimating generation time
+ELEVEN_LABS_CHARACTERS_PER_SECOND = 16.5
+OPEN_AI_CHARACTERS_PER_SECOND = 62.5
+UNREAL_SPEECH_CHARACTERS_PER_SECOND = 86.5
+
+try:
+    # Get AI argument from command line
+    # Specify which AI to use when determining cost: elevenlabs/openai/unrealspeech
+    SELECTED_AI_ARGUMENT = sys.argv[1]
+except IndexError:
+    print("Please provide an AI argument")
+    print(
+        "Usage: python3 src/determine-cost.py [elevenlabs/openai/unrealspeech]")
+    sys.exit()
+
+
+def determine_characters_per_second_based_on_ai():
+    switch = {
+        "elevenlabs": ELEVEN_LABS_CHARACTERS_PER_SECOND,
+        "openai": OPEN_AI_CHARACTERS_PER_SECOND,
+        "unrealspeech": UNREAL_SPEECH_CHARACTERS_PER_SECOND,
+    }
+    return switch.get(SELECTED_AI_ARGUMENT, "Invalid AI argument")
+
+characters_per_second = determine_characters_per_second_based_on_ai()
 
 with open('./data/test_data.txt', 'r') as file:
     file_length = len(file.read())
