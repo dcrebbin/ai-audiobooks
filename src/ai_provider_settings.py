62fa94096c1de33828d2b6a1c681f0a6be81eee7
Devon Crebbin <97231548+dcrebbin@users.noreply.github.com>
2023-12-19 20:32:46 +1000

    Updated project to support multiple AI providers (#5)
    * added multiple providers to determining the cost
    
    * updated env example
    
    * updated generation estimation time
    
    * implement ai provider setting config
    
    * refactored project use different ai providers
    
    * updated git ignore
    
    * change name example to 11labs
    
    * updated readme to explain new providers
    
    * updated gitignore

---
+++ src/ai_provider_settings.py
@@ -0,0 +1,77 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Model ID for the latest Elevenlabs text-to-speech API
+ELEVEN_LABS_MODEL_ID = "eleven_multilingual_v2"
+# Voice ID for the "ASMR" voice
+ELEVEN_LABS_VOICE_ID = "piTKgcLEGmPE4e6mEKli"
+ELEVEN_LABS_ENDPOINT = f"https://api.elevenlabs.io/v1/text-to-speech/{ELEVEN_LABS_VOICE_ID}"
+
+OPEN_AI_MODEL_ID = "tts-1"
+# Generic voice ID for OpenAI's API
+OPEN_AI_VOICE_ID = "alloy"
+OPEN_AI_ENDPOINT = "https://api.openai.com/v1/audio/speech"
+
+# Generic voice ID for Unreal Speech's API
+UNREAL_SPEECH_VOICE_ID = "Liv"
+UNREAL_SPEECH_ENDPOINT = "https://api.v6.unrealspeech.com/stream"
+
+AI_PROVIDER_SETTINGS = {
+    "elevenlabs": {
+        "url": ELEVEN_LABS_ENDPOINT,
+        "auth_type": "xi-api-key",
+        "auth_value": os.getenv("ELEVEN_LABS_API_KEY"),
+        "text_input_name": "text",
+        "payload": {
+            "model_id": ELEVEN_LABS_MODEL_ID,
+            "text": "",
+            "voice_settings": {
+                "similarity_boost": 0.95,
+                "stability": 0.85
+            }
+        }
+    },
+    "openai": {
+        "url": OPEN_AI_ENDPOINT,
+        "auth_type": "Authorization",
+        "auth_value": "Bearer " + os.getenv("OPEN_AI_API_KEY"),
+        "text_input_name": "input",
+        "payload": {
+            "model": OPEN_AI_MODEL_ID,
+            "input": "",
+            "voice": OPEN_AI_VOICE_ID,
+        }
+    },
+    "unrealspeech": {
+        "url": UNREAL_SPEECH_ENDPOINT,
+        "auth_type": "Authorization",
+        "auth_value": "Bearer " + os.getenv("UNREAL_SPEECH_API_KEY"),
+        "text_input_name": "Text",
+        "payload": {
+            #  This payload isn't a bug, it uses title case for the property names and the ints are strings
+            "Text":   "",
+            "VoiceId": "Liv",
+            "Bitrate": "64k",
+            "Speed":   "0",
+            "Pitch":   "1",
+            "Codec":   "libmp3lame",
+        }
+    }
+}
+
+
+def get_payload(ai_provider, text):
+    ai_config = AI_PROVIDER_SETTINGS.get(ai_provider)
+    payload = ai_config.get("payload")
+    payload[ai_config.get("text_input_name")] = text
+    return payload
+
+
+def get_auth(ai_provider):
+    ai_config = AI_PROVIDER_SETTINGS.get(ai_provider)
+    return {"type": ai_config.get("auth_type"), "value": ai_config.get("auth_value")}
+
+def get_url(ai_provider):
+    ai_config = AI_PROVIDER_SETTINGS.get(ai_provider)
+    return ai_config.get("url")
--- src/determine-cost.py
+++ src/determine-cost.py
@@ -1,8 +1,40 @@
+import sys;
+
+ELEVEN_LABS_COST_PER_1000_CHARACTERS = 0.3
 # Cost per 1000 characters only applies when your ElevenLabs account has exceeded your current tier
 # For more information on ElevenLabs up to date pricing visit: https://elevenlabs.io/subscription
 # Currently 30c (0.3 USD) per 1000 characters
-cost_by_character = 0.3/1000
 
+OPEN_AI_COST_PER_1000_CHARACTERS = 0.015
+# Cost per 1000 characters is applied to all requests to OpenAI's API
+# For more information on ElevenLabs up to date pricing visit: https://elevenlabs.io/subscription
+# Currently 0.015 USD per 1000 characters
+
+UNREAL_SPEECH_COST_PER_1000_CHARACTERS = 0.01
+# Cost per 1000 characters is a broad estimation based on UnrealSpeech's pricing page
+# For more information on ElevenLabs up to date pricing visit: https://unrealspeech.com/pricing
+# Currently 0.01 USD per 1000 characters
+
+try:
+    # Get AI argument from command line
+    # Specify which AI to use when determining cost: elevenlabs/openai/unrealspeech
+    SELECTED_AI_ARGUMENT = sys.argv[1]
+except IndexError:
+    print("Please provide an AI argument")
+    print("Usage: python3 src/determine-cost.py [elevenlabs/openai/unrealspeech]")
+    sys.exit()
+
+def determine_cost_based_on_ai():
+    switch = {
+        "elevenlabs": ELEVEN_LABS_COST_PER_1000_CHARACTERS,
+        "openai": OPEN_AI_COST_PER_1000_CHARACTERS,
+        "unrealspeech": UNREAL_SPEECH_COST_PER_1000_CHARACTERS,
+    }
+    return switch.get(SELECTED_AI_ARGUMENT, "Invalid AI argument")
+
+cost_by_character = determine_cost_based_on_ai() / 1000
+ 
+# Estimate the cost of generating a given text file
 with open('./data/test_data.txt', 'r') as file:
     file_length = len(file.read())
     total_cost = file_length * cost_by_character
